1769e646f371df7fc4149c2bf990c57b4dcabae4
ithzalumbra <thrash_omaximo@hotmail.com>
2021-09-22 12:20:23 -0300

    agrega aliases

--- commands/invite.js
+++ commands/invite.js
@@ -6,8 +6,8 @@ module.exports = {
   execute(message) {
     return message.member
       .send(
-        `https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=70282305&scope=bot
-    `
+        "lmao no"
+        //`https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=70282305&scope=bot`
       )
       .catch(console.error);
   }
--- commands/shuffle.js
+++ commands/shuffle.js
@@ -3,6 +3,7 @@ const i18n = require("../util/i18n");
 
 module.exports = {
   name: "shuffle",
+  aliases: ["random"],
   description: i18n.__("shuffle.description"),
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
